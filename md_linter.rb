b75b8ff4d21fc518fef7e92749cb083ae95483aa
Brian Hedberg <bshslowfood@gmail.com>
2016-12-06 11:24:30 -0600

    warning flag in readme

--- md_linter.rb
+++
@@ -1,7 +0,0 @@
-violating_words = [ '18Fers', 'US Government' ].freeze
-
-File.open("test.md").each do | line |
-	violating_words.each do | word |
-		puts "found: #{word}" if line =~ /#{word}/
-	end
-end
--- test.md
+++
@@ -1,6 +0,0 @@
-This is a test markdown file
-
-## Heading
-18Fers
-US Government
-18Fers in the US Government
