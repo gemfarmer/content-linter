9909468b27892e1c36a316f7a9fcf7792663427c
Brian Hedberg <bshslowfood@gmail.com>
2017-01-25 13:40:05 -0600

    configure for cloud foundry

--- spec/services/remark_processor_spec.rb
+++ spec/services/remark_processor_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe RemarkProcessor do
   it 'it can process a file and return an object' do
     remark_processor = RemarkProcessor.new('Hey guys {% something %}')
-    expect(remark_processor.process).to eq RETEXT_RESPONSE
+    expect(remark_processor.process).to match RETEXT_RESPONSE
   end
 
   it 'it formates the processed response' do
@@ -14,20 +14,54 @@ describe RemarkProcessor do
       message: '`guys` may be insensitive, use `people`, `persons`, `folks` instead'
     }]
 
-    expect(remark_processor.format(RETEXT_RESPONSE)).to eq formatted_response
+    expect(remark_processor.format(RETEXT_RESPONSE)).to match formatted_response
   end
 
-  it 'it returns a list of errors' do
+  it 'suggests more appropriate content (retext-equality)' do
     remark_processor = RemarkProcessor.new('# Hey guys {% something %}')
-
-    allow(remark_processor).to receive(:process).
-      and_return(RETEXT_RESPONSE)
 
     formatted_response = [{
       line: 1,
       message: '`guys` may be insensitive, use `people`, `persons`, `folks` instead'
     }]
 
-    expect(remark_processor.result).to eq formatted_response
+    expect(remark_processor.result).to match formatted_response
+  end
+
+  it 'checks for misspellings (retext-spell)' do
+    remark_processor = RemarkProcessor.new("Something in this\n sentense is not spelled correctly.")
+
+    formatted_response = [{
+      line: 2,
+      message: '`sentense` is misspelt; did you mean `sentence`?'
+    }]
+
+    expect(remark_processor.result).to match formatted_response
+  end
+
+  it 'checks for better use of quotes (retext-quote)' do
+    remark_processor = RemarkProcessor.new("Something' is up,\n isnt it?")
+
+    formatted_response = [{
+      line: 2,
+      message: "`isnt` is misspelt; did you mean `isn't`, `int`, `inst`?"
+    },
+    {
+      line: 1,
+      message: "Expected a smart quote: `’`, not `'`"
+    }]
+
+    expect(remark_processor.result).to match formatted_response
+  end
+
+  it 'makes suggestions for better words (retext-simplify)' do
+    remark_processor = RemarkProcessor.new("This test is positively easy to utilize!")
+
+    formatted_response = [{
+      line: 1,
+      message: "Replace “utilize” with “use”"
+    }]
+
+    expect(remark_processor.result).to match formatted_response
   end
 end
